bbe810240650b76fb0e8ed38600a84c92455a98a
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2021-11-26 15:33:05 -0800

    hopefully works
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+<center>
+    <h1>Eons</h1>
+    <i>Through eons we fly,</i><br>
+    <i>through eons we fall</i>
+</center>
+<br>
+
+Write. Program. Draw. Compose. _Create_.
+
+Eons is a script that installs a variety of (mostly) open-source tools given a clean Windows 11 install. It's intended for people who exercise all parts of the creative process regularly.
+
+## Running
+
+On a clean Windows 11 machine, open `powershell` with adminstrative privileges. This is of course dangerous, but Eons _is_ a script to install software. _Arc'blroth is not responsible for any damages incurred by this script_.
+
+In that powershell window, run
+```
+.\eons.ps1
+```
---
+++ eons.ps1
@@ -0,0 +1,87 @@
+Write-Output "__ __      __ "
+Write-Output "|_ /  \|\ |(_ "
+Write-Output "|__\__/| \|__)"
+
+$ErrorActionPreference = "Stop"
+$ProgressPreference = 'SilentlyContinue'
+
+$EonsCacheFile = "$PSScriptRoot/.eonscache"
+$EonsPackagesFile = "$PSScriptRoot/packages.json"
+$EonsInstallCacheFile = "$EonsCacheFile/.installed"
+$EonsBinFile = "$USERPROFILE/bin"
+
+# Create required directories
+if(-Not(Test-Path -Path $EonsCacheFile)) {
+    $null = New-Item -Path $EonsCacheFile -ItemType Directory
+}
+if(-Not(Test-Path -Path $EonsInstallCacheFile)) {
+    $null = New-Item -Path $EonsInstallCacheFile -ItemType File
+}
+if(-Not(Test-Path -Path $EonsBinFile)) {
+    $null = New-Item -Path $EonsBinFile -ItemType Directory
+}
+
+$env:Path += $EonsCacheFile
+
+# Read cache and packages
+$EonInstallCache = Get-Content $EonsInstallCacheFile
+if($null -eq $EonInstallCache) {
+    $EonInstallCache = @()
+}
+
+$EonPackages = Get-Content $EonsPackagesFile | ForEach-Object {
+    if($_.Trim().StartsWith("//")) {
+        ""
+    } else {
+        $_
+    }
+} | ConvertFrom-Json
+
+# Enumerate packages and install each
+$EonPackages | ForEach-Object {
+    # Skip already installed packages
+    $package = $_
+    if(-Not($EonInstallCache.Contains($package.name))) {
+        switch($package.type) {
+            "winget" {
+                Write-Output ("Installing package " + $package.package)
+                winget install -e --id $package.package
+                if($LastExitCode -ne 0) {
+                    throw ("Failed to install package " + $package.package)
+                }
+                break
+            }
+            "copy" {
+                Write-Output ("Copying " + $package.from + " to " + $package.to)
+                Copy-Item -Path $package.from -Destination $package.to
+                break
+            }
+            "download" {
+                Write-Output ("Downloading " + $package.url)
+                $to = $EonsCacheFile
+                if($null -ne $package.to) {
+                    $to = $package.to
+                }
+                $fileName = [System.IO.Path]::GetFileName($package.url)
+                Invoke-WebRequest -UseBasicParsing -Uri $package.url -OutFile ($to + "/" + $fileName)
+                if($null -ne $package.sha256) {
+                    $expectedContent = (Invoke-WebRequest -UseBasicParsing -Uri $package.sha256).Content
+                    $expected = [System.Text.Encoding]::ASCII.GetString($expectedContent).ToLower().Trim()
+                    $actual = (Get-FileHash ($to + "/" + $fileName) -Algorithm "SHA256").Hash.ToLower()
+                    if($expected -ne $actual) {
+                        throw ("Failed to verify download hash for " + $package.url)
+                    }
+                }
+                break
+            }
+            "run" {
+                Write-Output ("Running " + $package.command)
+                Invoke-Expression $package.command
+                if($LastExitCode -ne 0) {
+                    throw ("Failed to run command " + $package.command)
+                }
+            }
+        }
+        Add-Content -Path $EonsInstallCacheFile -Value $_.name
+    }
+}
